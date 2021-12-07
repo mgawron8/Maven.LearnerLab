19ec8c747d8f954facdf544dfa607a8c77aef661
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-12-07 08:48:23 -0500

    current progress on lab, on 3.1

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,23 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    public final Long id;
+    public String name;
+
+    public Person(Long id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return  this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,32 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void ConstructorTest(){
+        Long expectedId = 1L;
+        String expectedName = "Mike";
+
+        Person test = new Person(expectedId,expectedName);
+
+        Assert.assertEquals(expectedId, test.getId());
+        Assert.assertEquals(expectedName,test.getName());
+    }
+
+    @Test
+    public void setNameTest(){
+        Long expectedId = 1L;
+        String oldName = "Mike";
+        String newName = "notMike";
+
+        Person test = new Person(expectedId,oldName);
+        test.setName(newName);
+
+        Assert.assertNotEquals(oldName,test.getName());
+        Assert.assertEquals(newName,test.getName());
+    }
 
 }
